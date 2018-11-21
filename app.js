bc775dbf55137b081f1e5bd0df0cd29f434d2cca
workspaceproject06 <42857670+workspaceproject06@users.noreply.github.com>
2018-11-21 15:29:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function(){
+$("button").click(function(){
+$('#large-vp').fadeIn(2000).fadeOut(10000).css('font-family','cursive');
+});
+});
+
+		
+$(function(){
+$("button").click(function(){
+$('#small-vp').fadeIn(2000).fadeOut(10000).css('font-family','cursive');
+});
+});
+
+		
