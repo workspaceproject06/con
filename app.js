db4eed3fb11547a8acaeb054218957cb741f8802
workspaceproject06 <42857670+workspaceproject06@users.noreply.github.com>
2018-11-21 16:29:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function(){
 $("button").click(function(){
-$('#large-vp').fadeIn(2000).fadeOut(10000).css('font-family','cursive');
+$('#hidden').fadeIn(2000).fadeOut(10000).css('font-family','cursive');
 });
 });
 
-		
+	
